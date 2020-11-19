af8cc30179acdb5cc674ba48cd7925c688cd879a
Prajwal <prajwalpareek112211@gmail.com>
2020-11-20 02:14:42 +0530

    first commit

---
+++ cal.js
@@ -0,0 +1,40 @@
+function myFunction1(){
+    var firstVal = prompt("Enter first value");
+    var secondVal = prompt("Enter second value");
+    var thirdVal = alert('Multiplication of both number is '+firstVal*secondVal);
+}
+function myFunction2(){
+    var firstVal = prompt("Enter first value");
+    var secondVal = prompt("Enter second value");
+    var sub = parseInt(firstVal) - parseInt(secondVal)
+    var thirdVal = alert('Substraction of both number is '+ sub);
+}
+function myFunction3(){
+    var firstVal = prompt("Enter first value");
+    var secondVal = prompt("Enter second value");
+    var sum = parseInt(firstVal) + parseInt(secondVal);
+    var thirdVal = alert('Addition of both number is '+ sum);
+}
+function myFunction4(){
+    var firstVal = prompt("Enter first value");
+    var secondVal = prompt("Enter second value");
+    var div = parseInt(firstVal) / parseInt(secondVal);
+    var thirdVal = alert('Division of both number is '+ div);
+}
+function myFunction5(){
+    var firstVal = prompt("Enter first value");
+    var secondVal = prompt("Enter second value");
+    var per = (parseInt(firstVal) / parseInt(secondVal))*100;
+    var thirdVal = alert('Percentage of both number is '+ per);
+}
+function myFunction6(){
+    var firstVal = prompt("Enter first value");
+    var secondVal = prompt("Enter second value");
+    var modu = (parseInt(firstVal) % parseInt(secondVal));
+    var thirdVal = alert('Modulas of both number is '+ modu);
+}
+function myFunction7(){
+    var firstVal = prompt("Enter first value");
+    var sqr = (parseInt(firstVal) * parseInt(firstVal));
+    var thirdVal = alert('Modulas of both number is '+ sqr);
+}
